bab224fcaef1da9279656ac8a322b75d26cbc4f8
Stefan Larsson <stefan@ngti.nl>
2018-06-27 17:44:25 +0200

    And now starting with the pre-trained weights instead of none, should work way better

--- train-trafficlights/train-yolov3-tiny.sh
+++ train-trafficlights/train-yolov3-tiny.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -ex
-../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg
+../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg ../../darknet/darknet53.conv.74
 #  -gpus 0
-#  ../../darknet/yolov3-tiny.weights
