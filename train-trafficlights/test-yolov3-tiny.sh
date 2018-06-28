02a8b46f3e17e738808c129d731b7a2334726ae7
Stefan Larsson <stefan@ngti.nl>
2018-06-28 18:21:01 +0200

    Continue training and test from 200-iterations backup weights

--- train-trafficlights/test-yolov3-tiny.sh
+++ train-trafficlights/test-yolov3-tiny.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ex
-../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_100.weights green2.jpg
+../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_200.weights green2.jpg
--- train-trafficlights/train-yolov3-tiny.sh
+++ train-trafficlights/train-yolov3-tiny.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 set -ex
-../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg ../../darknet/darknet53.conv.74
+#../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg ../../darknet/darknet53.conv.74
+../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg backup/yolov3-tiny-trafficlights-train_200.weights
 #  -gpus 0
