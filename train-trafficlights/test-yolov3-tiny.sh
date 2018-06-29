cfb90d65ad5350e874844a8303a102518cbe7358
Stefan Larsson <stefan@ngti.nl>
2018-06-29 12:12:02 +0200

    Start training using proper pre-trained YOLOv3-tiny weights (imagenet). Increase max batch size to beyond trained-to amount. Train with GPU.

--- train-trafficlights/test-yolov3-tiny.sh
+++ train-trafficlights/test-yolov3-tiny.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 set -ex
-../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_500.weights red.jpg -thresh 0
+LD_LIBRARY_PATH=/usr/local/cuda/lib/ ../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_500.weights red.jpg -thresh 0
+#for i in just_traffic_lights/*.jpg; do
+#  ../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_500.weights $i -thresh 0
+#done
--- train-trafficlights/train-yolov3-tiny.sh
+++ train-trafficlights/train-yolov3-tiny.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
 #../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg ../../darknet/darknet53.conv.74
-../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg backup/yolov3-tiny-trafficlights-train_200.weights
+LD_LIBRARY_PATH=/usr/local/cuda/lib/ ../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg ../../darknet/yolov3-tiny.weights
+#../../darknet/darknet detector train cfg-voc.data yolov3-tiny-trafficlights-train.cfg backup/yolov3-tiny-trafficlights-train_200.weights
 #  -gpus 0
