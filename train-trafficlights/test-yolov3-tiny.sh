4cc4b54fda15aca6c31e7be85b6efd64365ebf11
Stefan Larsson <stefan@ngti.nl>
2018-06-29 09:20:45 +0200

    Loss is about 1.5 at 500 iterations, still fails to detect test images :(

--- train-trafficlights/test-yolov3-tiny.sh
+++ train-trafficlights/test-yolov3-tiny.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -ex
-../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_200.weights green2.jpg
+../../darknet/darknet detector test cfg-voc.data yolov3-tiny-trafficlights-test.cfg backup/yolov3-tiny-trafficlights-train_500.weights red.jpg -thresh 0
